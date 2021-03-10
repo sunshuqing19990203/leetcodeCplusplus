a77a11d2ec9c4f78ab4083d80844990f85fd7366
sunshuqing19990203 <3356014655@qq.com>
2021-03-11 00:07:50 +0800

    0310BST总结-1

---
+++ 654最大二叉树.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    // 在左闭右开区间[left, right)，构造二叉树
+    TreeNode* traversal(vector<int>& nums, int left, int right) {
+        if (left >= right) return nullptr;
+
+        // 分割点下表：maxValueIndex
+        int maxValueIndex = left;
+        for (int i = left + 1; i < right; ++i) {
+            if (nums[i] > nums[maxValueIndex]) maxValueIndex = i;
+        }
+
+        TreeNode* root = new TreeNode(nums[maxValueIndex]);
+
+        // 左闭右开：[left, maxValueIndex)
+        root->left = traversal(nums, left, maxValueIndex);
+
+        // 左闭右开：[maxValueIndex + 1, right)
+        root->right = traversal(nums, maxValueIndex + 1, right);
+
+        return root;
+    }
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return traversal(nums, 0, nums.size());
+    }
+};
+
+作者：carlsun-2
+链接：https://leetcode-cn.com/problems/maximum-binary-tree/solution/654-zui-da-er-cha-shu-gen-ju-shu-zu-gou-q0hp9/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
